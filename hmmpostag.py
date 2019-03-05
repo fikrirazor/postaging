c9e34568b2a2618f28e7d4efa39052d5661b6b89
fikrirazor <fikrirazor@outlook.co.id>
2019-03-06 03:28:06 +0700

    hitung masing masing jumlah kata dan tag

--- hmmpostag.py
+++ hmmpostag.py
@@ -8,4 +8,10 @@ Created on Wed Mar  6 02:57:00 2019
 import pandas as pd
 
 file = r'idn-tagged-corpus-master/Indonesian_Manually_Tagged_Corpus.tsv'
-data =  pd.read_table(file,header=None)
+dt =  pd.read_table(file,header=None)
+
+#tes=dt.apply(pd.value_counts)
+hk=dt.iloc[:,0].value_counts()
+ht=dt.iloc[:,1].value_counts()
+#hk.plot('barh')
+#ht..plot('barh')
