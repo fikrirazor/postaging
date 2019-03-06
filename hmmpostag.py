be36d83eaa7db4e00ed1297fe4f776b75534d2de
fikrirazor <fikrirazor@outlook.co.id>
2019-03-07 04:43:11 +0700

    update

--- hmmpostag.py
+++ hmmpostag.py
@@ -6,12 +6,18 @@ Created on Wed Mar  6 02:57:00 2019
 """
 
 import pandas as pd
-
+from collections import Counter
 file = r'idn-tagged-corpus-master/Indonesian_Manually_Tagged_Corpus.tsv'
 dt =  pd.read_table(file,header=None)
 
+X=dt.iloc[:,0]
+Y=dt.iloc[:,1]
 #tes=dt.apply(pd.value_counts)
-hk=dt.iloc[:,0].value_counts()
-ht=dt.iloc[:,1].value_counts()
+#hk=X.value_counts()
+#ht=Y.value_counts()
 #hk.plot('barh')
 #ht..plot('barh')
+table=pd.crosstab(X,Y)
+cnames=table.columns.values
+peluangemisi=table/table[cnames].sum()
+
