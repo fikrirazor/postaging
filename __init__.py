84db566f14cea21f97c6c4b14b04fa11e88ca290
fikrirazor <fikrirazor@outlook.co.id>
2019-03-07 22:07:10 +0700

    update

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ Created on Wed Mar  6 02:57:00 2019
 
 import pandas as pd
 import numpy as np
-import monsterautomata as ma
+import hmm as h
 
 file = r'idn-tagged-corpus-master/Indonesian_Manually_Tagged_Corpus.tsv'
 dt =  pd.read_table(file,header=None)
@@ -17,5 +17,5 @@ Y=dt.iloc[0:1000,1]
 c=np.asarray(Y.iloc[1:1000])
 f=np.asarray(Y.iloc[0:999])
 
-peluangemisi=ma.pt(X,Y)
-peluangtransisi=ma.pt(f,c)
+peluangemisi=h.pt(X,Y)
+peluangtransisi=h.pt(f,c)
