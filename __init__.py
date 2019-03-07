8d5959c931fb5b0ce1798bc5b2108b4725872533
fikrirazor <fikrirazor@outlook.co.id>
2019-03-08 03:33:43 +0700

    update

--- __init__.py
+++ __init__.py
@@ -26,31 +26,5 @@ peluangtransisi=h.pt(f,c) #P(Yt|Yt-1)
 y=np.asarray(peluangawal)
 A=np.asarray(peluangtransisi)
 B=np.asarray(peluangemisi).T
-def viterbi(y, A, B, Pi=None):
-    # Cardinality of the state space
-    K = A.shape[0]
-    # Initialize the priors with default (uniform dist) if not given by caller
-    Pi = Pi if Pi is not None else np.full(K, 1 / K)
-    T = len(y)
-    T1 = np.empty((K, T), 'd')
-    T2 = np.empty((K, T), 'B')
-
-    # Initilaize the tracking tables from first observation
-    T1[:, 0] = Pi * B[:, y[0]]
-    T2[:, 0] = 0
-
-    # Iterate throught the observations updating the tracking tables
-    for i in range(1, T):
-        T1[:, i] = np.max(T1[:, i - 1] * A.T * B[np.newaxis, :, y[i]].T, 1)
-        T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
-
-    # Build the output, optimal model trajectory
-    x = np.empty(T, 'B')
-    x[-1] = np.argmax(T1[:, T - 1])
-    for i in reversed(range(1, T)):
-        x[i - 1] = T2[x[i], i]
-
-    return x, T1, T2
-
      
-gg=viterbi(y,A,B)
+gg=h.viterbi(y,A,B)
